a5333a329fef90c7ed5041241cb32a7d812295a3
Dymas F. Antunes Ribeiro d. Santos <dymas10felipe20@gmail.com>
2025-01-31 14:44:00 -0300

    new

--- config.js
+++ config.js
@@ -4,3 +4,18 @@ document.addEventListener("DOMContentLoaded", function () {
   }
   window.scrollTo(0, 0);
 });
+
+
+// ----------------- Janela Menu -----------------
+
+document.getElementById('menu-button').addEventListener('click' , function
+() {
+    var menuContet = document.getElementById('menu-content');
+    if (menuContet.style.display === 'block') {
+      menuContet.style.display = 'none';
+    } else {
+      menuContet.style.display = 'block';
+      this.textConten = 'Fechar Menu'
+    }
+
+});
