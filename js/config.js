fd97b94d56c92ffdbfcf7907a71c836f55ced495
Dymas F. Antunes Ribeiro d. Santos <124197980+oDyKoz@users.noreply.github.com>
2026-03-06 19:31:00 -0300

    feat: Conclusão de upgrades do portfolio

--- js/config.js
+++ js/config.js
@@ -4,20 +4,3 @@ document.addEventListener("DOMContentLoaded", function () {
   }
   window.scrollTo(0, 0);
 });
-
-
-// ----------------- Janela Menu -----------------
-
-document.getElementById('menu-float').addEventListener('click' , function() 
-  {
-    var menuContet = document.getElementById('menu-list');
-    if (menuContet.classList.contains('flex')) {
-      menuContet.classList.add('hidden');
-      menuContet.classList.remove('flex');
-    } else {
-      menuContet.classList.add('flex');
-      menuContet.classList.remove('hidden');
-    }
-
-  }
-);
