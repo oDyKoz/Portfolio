5d5c0054233ac01bf4f7050d39cf1df18c5a948e
Dymas F. Antunes Ribeiro d. Santos <124197980+oDyKoz@users.noreply.github.com>
2026-03-07 20:31:48 -0300

    fix: Bug na responsividade mobile

--- js/config.js
+++ js/config.js
@@ -4,3 +4,17 @@ document.addEventListener("DOMContentLoaded", function () {
   }
   window.scrollTo(0, 0);
 });
+
+// Menu toggle
+const menuToggle = document.getElementById("menu"); 
+const nav = document.querySelector("nav ul");
+
+menuToggle.addEventListener("click", function () {
+  if (nav.classList.contains("flex")) {
+    nav.classList.remove("flex");
+    nav.classList.add("hidden");
+  } else {
+    nav.classList.add("flex");
+    nav.classList.remove("hidden");
+  }
+});
